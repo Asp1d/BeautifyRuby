4509d00f496fb78b0faf9caa43757bbc5c75ca05
Yanis Tsaunya <yanis88@gmail.com>
2015-04-02 20:16:07 +0300

    Revert "updated erb beautifier to work with last htmlbeautifier gem"

--- lib/erbbeautify.rb
+++ lib/erbbeautify.rb
@@ -1,7 +1,15 @@
 require 'htmlbeautifier'
 
 def beautify(input, output)
-  output.write(HtmlBeautifier.beautify(input))
+  dest = ""
+  beautifier = HtmlBeautifier::Beautifier.new(dest)
+  begin
+    beautifier.scan(input)
+    dest << "\n"
+  rescue
+    dest = ""
+  end
+  output.write(dest)
 end
 
 beautify $stdin.read.force_encoding('utf-8'), $stdout
